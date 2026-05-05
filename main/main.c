9f359fed711a3d8a6940c500fc434c4294eb3afd
Julian V <j.vilfort01@gmail.com>
2026-05-04 23:04:51 -0400

    Create task shared resources

--- main/main.c
+++ main/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "drone_common.h"
+
+// Shared controls packet
+control_packet_t control_packet;
+SemaphoreHandle_t controlPacketMutexHandle;
+
+// Shared telemetry packet
+telemetry_packet_t telemetry_packet;
+SemaphoreHandle_t telemetryPacketMutexHandle;
 
 void app_main(void)
 {
---
+++ main/tasks/controls_receive_task.c
@@ -0,0 +1,36 @@
+#include "esp_log.h"
+#include "esp_now.h"
+#include "freertos/FreeRTOS.h"
+#include "drone_common.h"
+#include "config.h"
+
+static const char *TAG = "Receive_Task";
+
+extern control_packet_t control_packet;
+extern SemaphoreHandle_t controlPacketMutexHandle;
+
+void controlsReceiveTask( void *pvParameters ) {
+  static esp_err_t ret;
+  static uint32_t lock_fail_count;
+  static control_packet_t control_packet_in;
+  static const uint8_t peer_addr[6] = ESPNOW_PEER_ADDR_CONF;
+
+  configASSERT(controlPacketMutexHandle);
+
+  while(1){
+    if (xSemaphoreTake(controlPacketMutexHandle, pdMS_TO_TICKS(3)) == pdFALSE) {
+      lock_fail_count++;
+      ESP_LOGI(TAG, "Failed to take mutex on attempt %u", lock_fail_count);
+      continue;
+    }
+
+    control_packet_in = control_packet;
+    xSemaphoreGive(controlPacketMutexHandle);
+
+    ret = esp_now_send(peer_addr, (uint8_t *) &control_packet, sizeof(control_packet));
+    if (ret != ESP_OK) {
+      ESP_LOGE(TAG, "Error transmitting control data: %s", esp_err_to_name(ret));
+    }
+    vTaskDelay(pdMS_TO_TICKS(5));
+  };
+};
